9f7a4c2a8422afc7d3e585815ed11a8b432b6209
brunaandradea <179890@unifio.edu.br>
2026-08-21 14:18:07 -0300

    adiciona testes e implementa camada courier

---
+++ Microservices/Courier-Management/Courier-Management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/Courier.java
@@ -0,0 +1,69 @@
+package com.algaworks.algadelivery.courier.management.domain.model;
+
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter(AccessLevel.PRIVATE)
+public class Courier {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String name;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String phone;
+
+    private Integer fulfilledDeliveriesQuantity;
+
+    private Integer pendingDeliveriesQuantity;
+
+    private OffsetDateTime lastFulfilledDeliveryAt;
+
+    private List<AssignedDelivery> pendingDeliveries = new ArrayList<>();
+
+    public List<AssignedDelivery> getPendingDeliveries() {
+        return Collections.unmodifiableList(this.pendingDeliveries);
+    }
+
+    public static Courier brandNew(String nome, String phone) {
+        Courier courier = new Courier();
+        courier.setId(UUID.randomUUID());
+        courier.setName(nome);
+        courier.setPhone(phone);
+        courier.setFulfilledDeliveriesQuantity(0);
+        courier.setPendingDeliveriesQuantity(0);
+        courier.setLastFulfilledDeliveryAt(null);
+
+        return courier;
+    }
+
+    public void assing(UUID deliveryId) {
+        this.pendingDeliveries.add(
+                AssignedDelivery.pendingDelivery(deliveryId)
+        );
+        this.pendingDeliveriesQuantity++;
+    }
+
+    public void fulfill(UUID deliveryId) {
+        AssignedDelivery delivery = this.pendingDeliveries.stream()
+                .filter(d -> d.getId().equals(deliveryId))
+                .findFirst()
+                .orElseThrow();
+        this.pendingDeliveries.remove(delivery);
+
+        this.pendingDeliveriesQuantity--;
+        this.fulfilledDeliveriesQuantity++;
+        this.lastFulfilledDeliveryAt = OffsetDateTime.now();
+    }
+
+}
